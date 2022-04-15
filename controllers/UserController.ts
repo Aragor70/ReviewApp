8fe89cb948ea6f6e84005cc5c56ef508ae7d3e86
Aragor70 <mikey.prus@gmail.com>
2022-04-15 20:19:06 +0000

    Server update: Include more user fields

--- controllers/UserController.ts
+++ controllers/UserController.ts
@@ -1,4 +1,6 @@
 import express, { Request, Response, Router, NextFunction } from "express";
+
+import gravatar from "gravatar";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 //custom imports
@@ -23,13 +25,47 @@ class UserController {
   register = asyncHandler(async (req: any, res: any, next: any) => {
     //check if password matches the confirmation password 2 form inputs
     if (req.body.password !== req.body.confPassword) {
-      return res.status(400).send({
-        message: "Password provided does not match the confirmation password.",
-      });
+      return next(new ErrorResponse("Password provided does not match the confirmation password.", 422));
     }
     try {
+
+      const { name, email, password } = req.body;
+      
+      let user
+
+      let avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm",
+      });
+
+      if (avatar && avatar.toString() && !avatar.toString().includes("https")) {
+        avatar = "https:" + avatar.toString();
+      }
+
+      const userName = (await name) || email.slice(0, email.indexOf("@"));
+
+      // encrypt password using bcrypt
+      const salt = await bcrypt.genSalt(10);
+
+      const safePassword = await bcrypt.hash(password, salt);
+
+      const key = ecGenerate.genKeyPair();
+      const public_key = key.getPublic("hex");
+      const private_key = key.getPrivate("hex");
+
+      user = {
+        name: userName,
+        email,
+        password: safePassword,
+        avatar: avatar || "",
+        public_key,
+        private_key,
+      }
+      
       // INSERT INTO accounts (name, email, password, avatar, public_key, private_key, account_type) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *
-      const createdUser = await User.create(req.body);
+      
+      const createdUser = await User.create(user);
       res.status(201).json({
         message: "User Created",
         success: true,
